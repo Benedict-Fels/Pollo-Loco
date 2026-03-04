799af4fab1e29d7416ac6f939014863fbfeea2e9
Benedict Fels <benedictfels@web.de>
2026-03-04 19:15:37 +0100

    restructured folder

--- script/audio.js
+++ script/audio.js
@@ -1,23 +1,21 @@
 
-const bossChickenDamageSound = new Audio('./sound/bossChicken/chicken_hurt.mp3');
-const bossChickenStompSound = new Audio('./sound/bossChicken/stomp.mp3');
-const eggCrackSound = new Audio('./sound/bossChicken/egg_crack.mp3');
-const chickenDeadSound = new Audio('./sound/chicken_dead.mp3');
-const bottleBreakSound = new Audio('./sound/bottle_break.mp3');
-const throwSound = new Audio('./sound/throw.mp3');
-const collectSound = new Audio('./sound/collect.wav');
-const music = new Audio('./sound/spagetti_western.ogg');
-const characterHurtSound = new Audio('./sound/character/character_damage.mp3');
-const characterDeadSound = new Audio('./sound/character/character_dead.wav');
+const soundFiles = {
+    bossChickenDamageSound: './sound/bossChicken/chicken_hurt.mp3',
+    bossChickenStompSound: './sound/bossChicken/stomp.mp3',
+    eggCrackSound: './sound/bossChicken/egg_crack.mp3',
+    chickenDeadSound: './sound/chicken_dead.mp3',
+    bottleBreakSound: './sound/bottle_break.mp3',
+    throwSound: './sound/throw.mp3',
+    collectSound: './sound/collect.wav',
+    characterHurtSound: './sound/character/character_damage.mp3',
+    characterDeadSound: './sound/character/character_dead.wav'
+}; 
+Object.keys(soundFiles).forEach(key => {
+    let audio = new Audio(soundFiles[key]); 
+    window[key] = audio; 
+    audio.volume = 0.15;
+});
 
-bossChickenDamageSound.volume = 0.25;
-bossChickenStompSound.volume = 0.25;
-eggCrackSound.volume = 0.25;
-bottleBreakSound.volume = 0.25;
-throwSound.volume = 0.25;
-collectSound.volume = 0.25;
-chickenDeadSound.volume = 0.25;
-characterHurtSound.volume = 0.25;
-characterDeadSound.volume = 0.25;
-music.volume = 0.125;
+const music = new Audio('./sound/spagetti_western.ogg');
+music.volume = 0.15;
 music.loop = true;
--- script/script.js
+++ script/script.js
@@ -6,9 +6,10 @@ let controlsWoodSign = false;
 let soundsWoodSign = false;
 let world;
 let keyboard;
+let volSettings = { general: 0.5, music: 0.5, effects: 0.5};
 
 function toggleFullscreen() {
-    let container = document.querySelector('.game-container'); // Wir nehmen den gesamten Container
+    let container = document.querySelector('.game-container');
     if (!document.fullscreenElement) {
         if (container.requestFullscreen) {
             container.requestFullscreen();
@@ -56,8 +57,31 @@ function processSoundsClick() {
 
 function openSoundsSign() {
     soundsTemplate();
+    requestAnimationFrame(() => {
+        document.getElementById('generalInputID').addEventListener('input', updateVolume);
+        document.getElementById('musicInputID').addEventListener('input', updateVolume);
+        document.getElementById('soundInputID').addEventListener('input', updateVolume);
+    })
     woodSignRef.classList.toggle('visible');
     soundsWoodSign = !soundsWoodSign;
+}
+
+function updateVolume() {
+    volSettings.general = document.getElementById('generalInputID').value / 100;
+    volSettings.music = document.getElementById('musicInputID').value / 100;
+    volSettings.effects = document.getElementById('soundInputID').value / 100;
+    let generalFactor = 0.3;
+    if (music) {
+        music.volume = volSettings.music * volSettings.general * generalFactor;
+    }
+    Object.keys(soundFiles).forEach(soundName => {
+        {
+            let audioObject = window[soundName];
+            if (audioObject) {
+                audioObject.volume = volSettings.effects * volSettings.general * generalFactor;
+            }
+        }
+    });
 }
 
 function startGame() {
@@ -83,16 +107,18 @@ function setOutroDiv(outcome) {
     setTimeout(() => {
         woodSignRef.style.backgroundImage = 'url(./img/9_intro_outro_screens/start/wooden_sign_top_small2.png)';
         woodSignRef.style.paddingTop = '9%'
-        woodSignRef.innerHTML = `<h3 class="restart-button" onclick="restartGame()">Return</h3>`    
+        woodSignRef.innerHTML = `<h3 class="restart-button" onclick="restartGame()">Return</h3>`
         woodSignRef.classList.add('visible');
     }, 1000);
 }
 
-function restartGame(){
+function restartGame() {
     console.log('restart Game');
     startDivRef.classList.remove('vis-none');
     outroDivRef.classList.add('vis-none');
     canvas.classList.add('vis-none');
+    woodSignRef.style.backgroundImage = 'url(./img/9_intro_outro_screens/start/wooden_sign_top2.png)';
+    woodSignRef.style.paddingTop = '16%'
     woodSignRef.classList.remove('visible');
     world = null;
 }
@@ -100,23 +126,23 @@ function restartGame(){
 function controlsTemplate() {
     woodSignRef.innerHTML = `
      <h2>Controls</h2>
-     <p>Left: a / ←</p >
-     <p>Right: d / →</p>
-     <p>Jump: space / w/ ↑</p>
-     <p>Throw Bottle: q</p>
-     <p>Attack: LMB / e</p>`
+     <div><p>Left: <span class="key-box"> A</span>  <span class="key-box"> ←</span></p>
+     <p>Right: <span class="key-box"> D</span>  <span class="key-box"> →</span></p>
+     <p>Jump: <span class="key-box"> Space</span> <span class="key-box"> W</span> <span class="key-box"> ↑</span></p>
+     <p>Throw Bottle: <span class="key-box">Q</span></p>
+     <p>Attack: <span class="key-box">LMB</span> <span class="key-box">E</span></p>`
 }
 function soundsTemplate() {
     woodSignRef.innerHTML = `
      <h2>Sound</h2>
          <div class="sound-controller">
-             <span>General</span><input type="range" min="1" max="100" value="50">
+             <span>General</span><input id="generalInputID" type="range" min="0" max="100" value="${volSettings.general * 100}">
          </div>
          <div class="sound-controller">
-             <span>Music</span><input type="range" min="1" max="100" value="50">
+             <span>Music</span><input id="musicInputID" type="range" min="0" max="100" value="${volSettings.music * 100}">
          </div>
          <div class="sound-controller">
-             <span>Effects</span><input type="range" min="1" max="100" value="50">
+             <span>Effects</span><input id="soundInputID" type="range" min="0" max="100" value="${volSettings.effects * 100}">
          </div>`
 }
 
